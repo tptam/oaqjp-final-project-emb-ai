fb856e9ee728103d11a6c8bb08420f6d64d9540c
tptam <roseisroseisrose@gmail.com>
2024-06-10 18:04:04 -0400

    Add server.py for Task 6

---
+++ server.py
@@ -0,0 +1,27 @@
+"""
+Flask server app for emotion detection
+"""
+
+from flask import Flask, request, render_template
+from EmotionDetection.emotion_detection import emotion_detector
+
+app = Flask("Emotion Detector")
+
+@app.route("/emotionDetector")
+def detector():
+    """Returns emotion detection result in text"""
+    text_to_analyze = request.args.get("textToAnalyze")
+    result = emotion_detector(text_to_analyze)
+    message = "For the given statement, the system response is 'anger': " \
+    f"{result['anger']}, 'disgust': {result['disgust']}, 'joy': {result['joy']}" \
+    f" and 'sadness': {result['sadness']}." \
+    f" The dominant emotion is <strong>{result['dominant_emotion']}</strong>."
+    return message
+
+@app.route("/")
+def index():
+    """Returns html for default index page"""
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
