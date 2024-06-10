307b423c22d8888c932a85c2cb22cb55df54d46f
tptam <roseisroseisrose@gmail.com>
2024-06-10 18:16:47 -0400

    Add error handling for Task 7

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -17,6 +17,10 @@ def emotion_detector(text_to_analyze):
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     payload = { "raw_document": { "text": text_to_analyze } }
     response = requests.post(url, json = payload, headers = header, timeout=30)
+    if response.status_code == 400:
+        emotion = {"anger": None, "disgust": None, "fear": None, 
+                    "joy": None, "sadness": None, "dominant_emotion":None}
+        return emotion
     formatted_response = json.loads(response.text)
     emotion = formatted_response["emotionPredictions"][0]["emotion"]
     emotion["dominant_emotion"] = max(emotion, key=emotion.get)
